2731b362862f92ac4c1e3acd330de009f2fe456a
Marceau-h <gti@marceau-h.fr>
2023-10-17 17:30:31 +0200

    kill launcher_STT.py when exiting the script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,17 @@
+control_c() {
+    kill "$PID"
+    exit
+}
+
+term() {
+    kill -SIGTERM "$PID"
+    exit
+}
+
+trap control_c SIGINT
+trap term SIGTERM
+
+
 if [ ! -d "venv" ]; then
     python3.11 -m venv venv
     pip install -Ur requirements.txt
@@ -11,5 +25,8 @@ fi
 source venv/bin/activate
 
 python launcher_STT.py &
+PID=$!
+echo "Started STT launcher with PID $PID"
 
 uvicorn api:app --port 5464 --host 0.0.0.0 --workers 4
+
