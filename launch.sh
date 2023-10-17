9c446e15f56b6e6b74dc71b41a57d658cdbce771
Marceau-h <gti@marceau-h.fr>
2023-10-17 16:33:21 +0200

    enforce non-default api key

--- launch.sh
+++ launch.sh
@@ -1,3 +1,13 @@
+if [ ! -d "venv" ]; then
+    python3.11 -m venv venv
+    pip install -Ur requirements.txt
+fi
+
+if [ ! "$STT_API_KEYS" ]; then
+    echo "STT_API_KEYS environment variable not set"
+    exit 1
+fi
+
 source venv/bin/activate
 
 python launcher_STT.py &
